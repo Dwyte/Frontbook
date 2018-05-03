f84b1686ab47ee20a2cd3454902b8ba8437f044c
Xander Martinez <martyymash@gmail.com>
2018-05-03 19:46:30 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-accounts = Array(10);
+accounts = Array(100000); //maximum number of accounts
 
 class account{
 
@@ -28,6 +28,55 @@ function StoreAccount(account){
 	}
 }
 
+function HashText(text){
+	let sumOfAscii = 0;
+	for (let i = 0; i < text.length; i++){
+		sumOfAscii += text.charCodeAt(i);
+	}
+	return sumOfAscii % accounts.length;
+}
+
+var databaseAccountsRef = firebase.database().ref().child("Accounts");
+
+function Login(){
+	let email = document.getElementById("login-email").value;
+	let pass = document.getElementById("login-pass").value;
+	let emailIndex = HashText(email);
+
+	var ref = firebase.database().ref("Accounts/" + emailIndex);
+		ref.once("value").then(function(snapshot) {
+			
+			if(email == "" || pass == ""){
+				alert("Please complete the form, thank you");
+				return;
+			}else{
+
+				// Check if the account exist
+				if(snapshot.child("email").val() == null){
+					alert("Account not found");
+					return;
+				}
+
+				// if the account exists, check if the password matched
+				if(snapshot.child("password").val() == pass){
+					localStorage.setItem("currAccName", snapshot.child("name").val());
+					localStorage.setItem("currAccDesc", snapshot.child("description").val());
+					localStorage.setItem("currAccBday", snapshot.child("birthday").val());
+					localStorage.setItem("currAccEmail", snapshot.child("email").val());
+					window.location.href = "./profile.html";
+				}else{
+					alert("Incorrect password");
+				}
+
+			}
+
+
+			//var email = snapshot.child("birthday").val();
+			});
+
+}
+
+
 function Register(){
 	let name = document.getElementById("name").value;
 	let bday = document.getElementById("bday").value;
@@ -35,8 +84,35 @@ function Register(){
 	let email = document.getElementById("email").value;
 	let pass = document.getElementById("pass").value;
 
+	if(name == "" || desc == "" || email == "" || pass == "" || bday == ""){
+		alert("Please complete the form, thank you!");
+		return;
+	}
+
 	let acc = new account(name, bday, desc, email, pass);
+	
+	localStorage.setItem("currAccName", acc.name);
+	localStorage.setItem("currAccDesc", acc.desc);
+	localStorage.setItem("currAccBday", acc.bday);
+	localStorage.setItem("currAccEmail", acc.email);
 
 	Hash(acc);
 	StoreAccount(acc);
+
+	// Store acc on the firebase database
+	databaseAccountsRef.child(acc.index).child("name").set(acc.name);
+	databaseAccountsRef.child(acc.index).child("birthday").set(acc.bday);
+	databaseAccountsRef.child(acc.index).child("description").set(acc.desc);
+	databaseAccountsRef.child(acc.index).child("email").set(acc.email);
+	databaseAccountsRef.child(acc.index).child("password").set(acc.pass);
+
+	window.location.href = "./profile.html"
+}
+
+function Logout(){
+	localStorage.setItem("currAccName", "");
+	localStorage.setItem("currAccDesc", "");
+	localStorage.setItem("currAccBday", "");
+	localStorage.setItem("currAccEmail", "");
+	window.location.href = "./index.html"
 }
